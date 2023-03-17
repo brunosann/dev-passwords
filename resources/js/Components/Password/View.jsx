cd16950c3cf7be113dbd8110d7b4d865d03ded7e
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2023-03-17 13:17:50 -0300

    feat (password): loading when viewing password

--- resources/js/Components/Password/View.jsx
+++ resources/js/Components/Password/View.jsx
@@ -7,6 +7,7 @@ import { ItemView } from "./ItemView";
 
 export const View = ({ id, setId }) => {
   const [password, setPassword] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!id) return;
@@ -16,55 +17,73 @@ export const View = ({ id, setId }) => {
 
   const getPassword = useCallback(async () => {
     try {
+      setLoading(true);
       const response = await fetch(route("passwords.show", id));
       if (!response.ok) throw response;
       const responseJson = await response.json();
       setPassword(responseJson);
     } catch (error) {
       console.log("error", error);
+    } finally {
+      setLoading(false);
     }
   }, [id]);
 
   return (
     <Overlay title="Visualizar senha" open={!!id} setOpen={setId}>
-      {password ? (
-        <>
-          <ul className="flex flex-col gap-2">
-            <li>
-              <ItemView label="Nome" name={password.name} />
-            </li>
-            {password.username && (
+      <>
+        {password && (
+          <>
+            <ul className="flex flex-col gap-2">
               <li>
-                <ItemView label="Username" name={password.username} />
+                <ItemView label="Nome" name={password.name} />
               </li>
-            )}
-            <li>
-              <ItemView label="Senha" name={password.password} />
-            </li>
-            {password.description && (
+              {password.username && (
+                <li>
+                  <ItemView label="Username" name={password.username} />
+                </li>
+              )}
               <li>
-                <ItemView label="Descrição" name={password.description} />
+                <ItemView label="Senha" name={password.password} />
               </li>
+              {password.description && (
+                <li>
+                  <ItemView label="Descrição" name={password.description} />
+                </li>
+              )}
+            </ul>
+            {password.custom_fields.length >= 1 && (
+              <div className="mt-4 pt-4 border-t-2">
+                <p className="leading-6 text-gray-900 mb-2">
+                  Campos personalizados
+                </p>
+                <ul className="flex flex-col gap-2">
+                  {password.custom_fields.map((field) => (
+                    <ItemView
+                      key={field.name}
+                      label={field.name}
+                      name={field.value}
+                    />
+                  ))}
+                </ul>
+              </div>
             )}
-          </ul>
-          {password.custom_fields.length >= 1 && (
-            <div className="mt-4 pt-4 border-t-2">
-              <p className="leading-6 text-gray-900 mb-2">
-                Campos personalizados
-              </p>
-              <ul className="flex flex-col gap-2">
-                {password.custom_fields.map((field) => (
-                  <ItemView
-                    key={field.name}
-                    label={field.name}
-                    name={field.value}
-                  />
-                ))}
-              </ul>
+          </>
+        )}
+
+        {loading && (
+          <div className="animate-pulse">
+            <div className="flex-1 space-y-6 py-1">
+              {[1, 2, 3, 4, 5].map((i) => (
+                <div key={i} className="space-y-2">
+                  <div className="w-16 h-2 bg-gray-400" />
+                  <div className="h-2 bg-gray-400" />
+                </div>
+              ))}
             </div>
-          )}
-        </>
-      ) : null}
+          </div>
+        )}
+      </>
     </Overlay>
   );
 };
